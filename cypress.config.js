c1fb61b5b13ab1ac698f4e3c503567af2a14e763
Vuthlari0116 <kvmabaso2001@gmail.com>
2025-07-16 19:30:23 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,1 @@
-const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  projectId: 'yasyi4',
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
